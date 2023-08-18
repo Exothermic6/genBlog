2276310b24ce41f9e5c2580cb6879df27fd603c2
Alfred <alfredmayende3@gmail.com>
2023-08-18 11:41:29 +0300

    created the admin dashboard

--- backend/blogapp/models.py
+++ backend/blogapp/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.utils import timezone
 
+
+
+from django.db import models
+class Blog(models.Model):
+    blog_title=models.CharField(max_length=50)
+    blog_post=models.TextField(max_length=500)
+    # author=
+    date_published=models.DateTimeField(timezone.now())
+    def __str__():
+        return self.blogTitle
 # Create your models here.
